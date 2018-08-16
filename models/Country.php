19c5da59c47b801a850e806846f460ef3fc96755
Gurikin <igor.banchikov@gmail.com>
2018-08-16 08:31:20 +0300

    work with DB start

---
+++ models/Country.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gurik
+ * Date: 16.08.2018
+ * Time: 8:30
+ */
+
+namespace app\models;
+
+
+use yii\db\ActiveRecord;
+
+class Country extends ActiveRecord
+{
+
+}
